cc29e15cb39f6a1ab810051f12908aab8fa42299
BuildTools <unconfigured@null.spigotmc.org>
2018-10-20 19:50:44 -0300

    Added smooth scrolling to page anchors, and the start of the nav bar

---
+++ js/smoothScroll.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        // Use the number to specify milliseconds to reach the anchored point
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
