e4f78395878b94d6dd27c38cefa2ada2e28132e0
Khoa Dien <khoa.dien@hotmail.com>
2018-10-20 23:00:14 -0300

    Reformatted smoothScroll.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,20 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
   
-      if (this.hash !== "") {
+        if (this.hash !== "") {
 
-        event.preventDefault();
-  
-        var hash = this.hash;
-  
-        // Use the number to specify milliseconds to reach the anchored point
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          window.location.hash = hash;
-        });
-      } // End if
+            event.preventDefault();
+    
+            var hash = this.hash;
+    
+            // Use the number to specify milliseconds to reach the anchored point
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        } // End if
     });
-  });
+});
