2841d7ddfa7e3abf5e3f0bf751391d6da8b2fb78
Khoa Dien <khoa.dien@hotmail.com>
2018-10-24 12:49:03 -0300

    Added more formatted to active links and fixed smoothScroll

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
     // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+    $("a").on("click", function(event) {
   
         if (this.hash !== "") {
 
             event.preventDefault();
     
-            var hash = this.hash;
+            var target = $(this).attr("href");
+            var scrollToPosition = $(target).offset().top - 60;
     
             // Use the number to specify milliseconds to reach the anchored point
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
+            $("html").animate({
+                "scrollTop": scrollToPosition
             }, 800, function() {
-                window.location.hash = hash;
+                window.location.hash = target;
+                $("html").animate({"scrollTop": scrollToPosition}, 0);
             });
         } // End if
     });
