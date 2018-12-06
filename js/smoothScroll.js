594777ea57ba01f962a9e5b39619d9cd0a8b8d8e
Khoa Dien <khoa.dien@hotmail.com>
2018-12-05 21:14:31 -0400

    Fix smoothScroll to work with other browsers

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -10,9 +10,12 @@ $(document).ready(function() {
             var scrollToPosition = $(target).offset().top - 60;
     
             // Use the number to specify milliseconds to reach the anchored point
-            $("html").animate({"scrollTop": scrollToPosition}, 800, function() {
+            $("html, body").animate({"scrollTop": scrollToPosition}, 800, function() {
                 window.location.hash = target;
-                $("html").animate({"scrollTop": scrollToPosition}, 0);
+                $("html, body").animate({"scrollTop": scrollToPosition}, 0);
+                
+                // Remove hash from URL on address bar
+                history.pushState("", document.title, window.location.pathname + window.location.search);
             });
         } // End if
     });
